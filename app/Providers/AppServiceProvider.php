06a7512b9b0550cd93e94fe1bf03e0acaff14fe6
A Ram Pandey <shashwatpandey100@gmail.com>
2025-05-05 15:56:15 +0530

    trying to deploy on render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,19 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
-    {
-        //
+    { {
+            if (env('APP_ENV') == 'production') {
+                $this->app['request']->server->set('HTTPS', true);
+            }
+        }
     }
 }
